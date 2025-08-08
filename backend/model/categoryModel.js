46ea08eb3fa91032f9d1e2de01df714e79787d3b
AsadArmanKhan <asadarmankhanworkmail@gmail.com>
2025-08-08 17:22:59 +0530

    Latest commit

--- backend/model/categoryModel.js
+++ backend/model/categoryModel.js
@@ -1,33 +1,34 @@
 const mongoose = require("mongoose");
 
-const categorySchema = new mongoose.Schema({
+const categorySchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     slug: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     Image: {
-        type: String,
-        required: null
+      type: String,
+      required: null,
     },
     // createdAt: {
     //     type: Date,
     //     default: Date.now
     // },
     status: {
-        type: Boolean,
-        default: true
-    }
-},
-    {
-        timestamps: true,
-    }
-)
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const categoryModel = mongoose.model("Category", categorySchema);
 
